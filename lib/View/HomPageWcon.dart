56b3de79c8e6a365a8313d490ef506cb1f066c7f
OUOBA <95525621+OUOBADeveloppeur@users.noreply.github.com>
2024-02-12 19:21:09 +0000

    ajout de willscopp

--- lib/View/HomPageWcon.dart
+++ lib/View/HomPageWcon.dart
@@ -1,8 +1,14 @@
+import 'dart:io';
+
 import 'package:bebegrandi/Controller/chargementDesDonne.dart';
 import 'package:bebegrandi/Model/tour.dart';
 import 'package:bebegrandi/View/navDrawer.dart';
 import 'package:bebegrandi/View/section.dart';
+import 'package:bebegrandi/shared/colors.dart';
 import 'package:flutter/material.dart';
+
+import '../component/expandbutton.dart';
+import '../shared/custom_text.dart';
 
 class HomPageWcon extends StatefulWidget {
   const HomPageWcon({super.key});
@@ -11,43 +17,128 @@ class HomPageWcon extends StatefulWidget {
   State<HomPageWcon> createState() => _HomPageWconState();
 }
 
+bool exit = false;
+
 class _HomPageWconState extends State<HomPageWcon> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          leading: Builder(
-            builder: (context) {
-              return IconButton(
-                tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
-                onPressed: () => Scaffold.of(context).openDrawer(),
-                icon: const Icon(
-                  Icons.dehaze,
-                  color: Colors.black,
-                ),
-              );
-            },
-          ),
-        ),
-        drawer: const NavDrawer(),
-        body: FutureBuilder(
-          // Remplace le Future par le chargement réel des données depuis ta source de données
-          future: fetchData(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              // ignore: prefer_const_constructors
-              return CircularProgressIndicator();
-            } else if (snapshot.hasError) {
-              // ignore: prefer_const_constructors
-              return Text("Erreur de chargement des données");
-            } else if (snapshot.hasData) {
-              Welcome? welcome = snapshot.data;
+    //---------------------------------------scopp------------------------
 
-              return SectionG(section: welcome!);
-            } else {
-              return Text("Aucune donnée disponible");
-            }
+    //---------fin scop-------------------------
+    // ignore: deprecated_member_use
+    return WillPopScope(
+      onWillPop: () async {
+        showDialog(
+          context: context,
+          barrierDismissible: false,
+          builder: (context) {
+            return AlertDialog(
+              //   title: Image.asset('logo.jpg'),
+              backgroundColor: blanc(),
+              shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10.0))),
+              contentPadding:
+                  const EdgeInsets.only(top: 5.0, left: 0.0, right: 0.0),
+              content: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 15.0, right: 15.0),
+                    child: Divider(
+                      thickness: 1.0,
+                      height: 2.0,
+                      color: green(),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10.0,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(10.0, 5.0, 5.0, 10.0),
+                    child: Row(
+                      children: [
+                        Flexible(
+                          child: CustomText(
+                            "êtes-vous sûr de vouloir quitter l'application?",
+                            tex: TailleText(context).soustitre * 0.81,
+                            color: noir(),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              actions: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ExpandedButton(
+                      color1: red(),
+                      color2: blanc(),
+                      exit: exit,
+                      num: 1,
+                      text: "NON",
+                    ),
+                    const SizedBox(width: 20),
+                    ExpandedButton(
+                      color1: green(),
+                      color2: blanc(),
+                      exit: exit,
+                      num: 2,
+                      text: "OUI",
+                    ),
+                  ],
+                ),
+              ],
+            );
           },
-        ));
+        );
+        return exit;
+      },
+      child: SafeArea(
+        child: Scaffold(
+            //---------------------scopp------------------------------
+
+            //----------------------fin scop------------------------
+            appBar: AppBar(
+              leading: Builder(
+                builder: (context) {
+                  return IconButton(
+                    tooltip:
+                        MaterialLocalizations.of(context).openAppDrawerTooltip,
+                    onPressed: () => Scaffold.of(context).openDrawer(),
+                    icon: const Icon(
+                      Icons.dehaze,
+                      color: Colors.black,
+                    ),
+                  );
+                },
+              ),
+            ),
+            drawer: const NavDrawer(),
+            body: FutureBuilder(
+              // Remplace le Future par le chargement réel des données depuis ta source de données
+              future: fetchData(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  // ignore: prefer_const_constructors
+                  return CircularProgressIndicator();
+                } else if (snapshot.hasError) {
+                  // ignore: prefer_const_constructors
+                  return Text("Erreur de chargement des données");
+                } else if (snapshot.hasData) {
+                  Welcome? welcome = snapshot.data;
+
+                  return SectionG(section: welcome!);
+                } else {
+                  return Text("Aucune donnée disponible");
+                }
+              },
+            )),
+      ),
+    );
   }
 }
---
+++ lib/component/expandbutton.dart
@@ -0,0 +1,74 @@
+// ignore_for_file: file_names, must_be_immutable
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import '../../shared/custom_text.dart';
+
+class ExpandedButton extends StatefulWidget {
+  ExpandedButton(
+      {super.key,
+      required this.color1,
+      required this.color2,
+      required this.text,
+      required this.num,
+      required this.exit});
+  final Color color1;
+  final Color color2;
+  final String text;
+  final int num;
+
+  bool exit;
+
+  @override
+  State<ExpandedButton> createState() => _ExpandedButtonState();
+}
+
+class _ExpandedButtonState extends State<ExpandedButton> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * 0.25,
+      child: ElevatedButton(
+        onPressed: () {
+          if (widget.num == 1) {
+            // pour non ...
+            
+            Navigator.pop(context, false);
+            setState(() {
+              widget.exit = false;
+            });
+          } else {
+            // pour oui  ...
+            Navigator.pop(context, true);
+            SystemNavigator.pop();
+            setState(() {
+              widget.exit = true;
+            });
+          }
+        },
+        style: ButtonStyle(
+          foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+          backgroundColor: MaterialStateProperty.all<Color>(widget.color1),
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(25),
+            ),
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(5.0),
+          child: CustomText(
+            widget.text,
+            color: widget.color2,
+            tex: TailleText(context).contenu,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
