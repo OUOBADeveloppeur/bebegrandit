042cb9e161efccb60396d9ca4b7004c4b06bc70d
Lucas-Developper <124780723+Lucas-Developper@users.noreply.github.com>
2024-03-20 22:00:21 +0000

    Update chargementDesDonne.dart

--- lib/Controller/chargementDesDonne.dart
+++ lib/Controller/chargementDesDonne.dart
@@ -5,12 +5,7 @@ import 'package:bebegrandi/Model/tour.dart';
 import 'package:bebegrandi/donneSection/section_paludisme/Section.dart';
 import 'package:bebegrandi/donneSection/section_danger/Section.dart';
 import 'package:bebegrandi/donneSection/Section_alimentation/Section.dart';
-<<<<<<< HEAD
-import 'package:bebegrandi/donneSection/section_accidents/Section.dart';
-
-=======
 import 'package:bebegrandi/donneSection/section_suivi_de_enfant/Section.dart';
->>>>>>> ca443f14aea1128292a2e0eae71e754e39475de9
 import '../donneSection/section_developpement/Section.dart';
 
 Future<Welcome> fetchData() async {
@@ -22,12 +17,7 @@ Future<Welcome> fetchData() async {
       sectionPa(),
       sectionQ(),
       sectionP(),
-<<<<<<< HEAD
-      sectionAccident()
-
-=======
       sectionsuivi()
->>>>>>> ca443f14aea1128292a2e0eae71e754e39475de9
       // Ajoute d'autres sections si nécessaire
     ],
   );
