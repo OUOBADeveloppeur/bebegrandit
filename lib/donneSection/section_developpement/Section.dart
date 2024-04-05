319f0118a80cb6b53fa071b3ac94326bd320190e
OUOBA <95525621+OUOBADeveloppeur@users.noreply.github.com>
2024-04-05 08:40:06 +0000

    Update Section.dart

--- lib/donneSection/section_developpement/Section.dart
+++ lib/donneSection/section_developpement/Section.dart
@@ -6,11 +6,39 @@ import 'package:bebegrandi/donneSection/section_developpement/Section2/Section.d
 import 'package:bebegrandi/donneSection/section_developpement/Section3/Section.dart';
 import 'package:bebegrandi/donneSection/section_developpement/Section4/Section.dart';
 import 'package:bebegrandi/donneSection/section_developpement/Section5/Section.dart';
+import 'package:bebegrandi/donneSection/section_developpement/Section1/Page/Page2.dart';
+import 'package:bebegrandi/donneSection/section_developpement/Section1/Page/Page3.dart';
+import 'package:bebegrandi/donneSection/section_developpement/Section2/Page/Page1.dart'
+    as Section2;
+import 'package:bebegrandi/donneSection/section_developpement/Section2/Page/Page2.dart'
+    as Section2;
+import 'package:bebegrandi/donneSection/section_developpement/Section2/Page/Page3.dart'
+    as Section2;
+import 'package:bebegrandi/donneSection/section_developpement/Section2/Page/Page4.dart';
+import 'package:bebegrandi/donneSection/section_developpement/Section2/Page/Page5.dart';
+import 'package:bebegrandi/donneSection/section_developpement/Section2/Page/Page6.dart';
+import 'package:bebegrandi/donneSection/section_developpement/Section2/Page/Page7.dart';
+import 'package:bebegrandi/donneSection/section_developpement/Section2/Page/Page8.dart';
+import 'package:bebegrandi/donneSection/section_developpement/Section2/Page/Page9.dart';
+import 'package:bebegrandi/donneSection/section_developpement/Section2/Page/Page10.dart';
 
 Section section1() {
   return Section(id: 0, colors: 0xFF088886, page: [
     // Page de garde de la section 1 (Les chutes)
+
     page1(),
+    page2(),
+    page3(),
+    page1(),
+    page2(),
+    page3(),
+    page4(),
+    page5(),
+    page6(),
+    page7(),
+    page8(),
+    page9(),
+    page10(),
   ], menu: [
     Menu(
       id: 1,
