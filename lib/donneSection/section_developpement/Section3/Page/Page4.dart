e2260192faca0ba352cc1fa10f08a7c8c9ad8ae5
OUOBA <95525621+OUOBADeveloppeur@users.noreply.github.com>
2024-05-10 15:26:02 +0000

    Update Page4.dart

--- lib/donneSection/section_developpement/Section3/Page/Page4.dart
+++ lib/donneSection/section_developpement/Section3/Page/Page4.dart
@@ -5,43 +5,49 @@ Pages page18() {
   return Pages(
     id: 4,
     titre: "Le développement de l’enfant de 6 à 12 mois",
-    icon:"images/developpement/pag10/ic-01-01.png" ,
+    icon: "images/developpement/pag10/ic-01-01.png",
     imageDescription: [
       ImageDescription(
         id: 1,
         texte: 900,
-        position: '0', taille: 0,
+        position: '0',
+        taille: 0,
         description: "La vue",
         image: "",
       ),
       ImageDescription(
-        id: 2,taille: 0,
+        id: 2,
+        taille: 0,
         position: '3',
         description:
             "Dès 7-8 mois, Bébé reconnaît les personnes et les objets dans la pièce. "
             "Bébé ne peut pas bien voir les objets loin de lui.",
-        image: "images/developpement/pag10/ic-02-01.png",
+        image: "images/developpement/pag10/ic-04-01.png",
       ),
       ImageDescription(
-        id: 3,taille: 0,
+        id: 3,
+        taille: 0,
         position: '3',
         description: "Dès 9-10 mois, mon enfant a une vision améliorée.",
-        image: "images/developpement/pag10/ic-03-01.png",
+        image: "images/developpement/pag10/ic-04-01.png",
       ),
       ImageDescription(
-        id: 4,taille: 0,
+        id: 4,
+        taille: 0,
         position: '3',
         description: "Dès 11-12 mois, Bébé voit aussi bien qu’un adulte.",
         image: "images/developpement/pag10/ic-04-01.png",
       ),
       ImageDescription(
-        id: 5,taille: 0,
+        id: 5,
+        taille: 0,
         texte: 900,
         description: "Ce que je fais",
         image: "",
       ),
       ImageDescription(
-        id: 6,taille: 0,
+        id: 6,
+        taille: 0,
         position: '3',
         description:
             "- Entre 7 et 9 mois, je joue à cache-cache pour que mon enfant comprenne qu’un objet"
@@ -49,7 +55,8 @@ Pages page18() {
         image: "images/developpement/pag20/ic-03-01.png",
       ),
       ImageDescription(
-        id: 7,taille: 0,
+        id: 7,
+        taille: 0,
         position: '3',
         description:
             '''- j'evite d'exposer mon enfant à la lumière vive(écran de télévision,
@@ -59,13 +66,16 @@ Pages page18() {
       ImageDescription(
         id: 8,
         texte: 900,
-        position: '5', bg: 0xFFFFE4E1,
+        position: '5',
+        bg: 0xFFFFE4E1,
         description: "Attention",
         image: "images/developpement/pag9/ic-06-01.png",
       ),
       ImageDescription(
-        id: 9, bg: 0xFFFFE4E1,
-        position: '6',taille: 0,
+        id: 9,
+        bg: 0xFFFFE4E1,
+        position: '6',
+        taille: 0,
         description:
             "Lorsque mon enfant a les yeux qui coulent en permanence ou sont sensibles à la lumière, "
             "a une tâche banche au niveau des yeux, se cogne partout, "
@@ -73,9 +83,11 @@ Pages page18() {
         image: "",
       ),
       ImageDescription(
-        id: 10,taille: 0,
+        id: 10,
+        taille: 0,
         texte: 900,
-        color: 0xFFFF0000, bg: 0xFFFFE4E1,
+        color: 0xFFFF0000,
+        bg: 0xFFFFE4E1,
         position: '3',
         description: " je l’emmène au centre de santé",
         image: "images/developpement/pag9/ic-05-01.png",
